ad524aaa80948196873e3248b818d394adb76c33
blishchenko1981 <blishchenko1981@gmail.com>
2021-07-31 23:08:13 -0400

    work on boolean problem

--- src/SaturdayPractice/firstSatTask.java
+++ src/SaturdayPractice/firstSatTask.java
@@ -2,5 +2,42 @@ package SaturdayPractice;
 
 public class firstSatTask {
     // I m going to do here Abbas task
+/*
+Boolean b1=new Boolean ("true");
+Boolean  b2=new Boolean(true);
+System.out.println(b1.equals.(b2)); // What's the output
+Boolean b3=new Boolean("True");
+Boolean b4=new Boolean("tRue");
+System.out.println(b3.equals(b4));
+Boolean b5 =new Boolean("hey");
+Boolean b6=new Boolean("bey");
+System.out.println(b5.equals(b6));
+ */
+    public static void main(String[] args) {
+        Boolean b1=new Boolean ("true");
+        Boolean  b2=new Boolean(true);
+
+        System.out.println(b1);
+        System.out.println(b2);
+
+        System.out.println("+++++++++++++++++++++++++");
+        Boolean b3=new Boolean("True");
+        Boolean b4=new Boolean("tRue");
+
+        System.out.println(b3);
+        System.out.println(b4);
+        System.out.println(b3.equals(b4));
+        System.out.println("---------------------------");
+        Boolean b5 =new Boolean("TrUe");
+        Boolean b6=new Boolean("trua");
+        System.out.println(b5);
+        System.out.println(b6);
+        System.out.println(b5.equals(b6));
+
+    }
+
+
+
+
 
 }
